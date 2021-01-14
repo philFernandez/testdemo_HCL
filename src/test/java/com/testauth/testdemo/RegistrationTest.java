fded18e81b58fe91bc1aa998fcb4ee0d2729f34a
Phil Fernandez <norcal618@gmail.com>
2021-01-14 02:00:20 -0800

    done

---
+++ src/test/java/com/testauth/testdemo/RegistrationTest.java
@@ -0,0 +1,39 @@
+package com.testauth.testdemo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.testauth.testdemo.dao.UserDao;
+import com.testauth.testdemo.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationTest {
+
+    UserDao service;
+    User user;
+    @BeforeEach
+    public void setupUserService() {
+        service = new UserDao();
+        user = User.builder().username("Phil").password("123").build();
+        service.registerNewUser(user);
+    }
+
+    @Test
+    /**
+     * Make sure new user is added to in memory database when first registered
+     */
+    public void testInMemDB() {
+        assertEquals(true, User.getUsers().size() == 1);
+    }
+
+    @Test
+    public void testValidUserLogin() {
+        assertEquals(true, service.authenticateUser(user));
+    }
+
+    @Test 
+    public void testInvalidUserLogin() {
+        User invalidUser = User.builder().username("Fred").password("abc").build();
+        assertEquals(false, service.authenticateUser(invalidUser));
+    }
+    
+}
